959df32372f476cdd30d6674ee19c3190ae8ce2e
tyagipriyanshi <67687067+tyagipriyanshi@users.noreply.github.com>
2022-03-11 17:50:10 +0530

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+import CoinsTable from '../components/CoinsTable'
+
+function HomePage() {
+  return <>
+  <Banner/>
+  <CoinsTable/>
+  </>
+}
+
+export default HomePage
